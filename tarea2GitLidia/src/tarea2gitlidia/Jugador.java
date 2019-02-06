88949382de86ee6dd73f56e5254dd1ab75cfb940
lidia <lidiacintadog@gmail.com>
2019-02-06 18:03:22 +0100

    primer comit

---
+++ tarea2GitLidia/src/tarea2gitlidia/Jugador.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2gitlidia;
+
+/**
+ *
+ * @author bicho
+ */
+public class Jugador {
+
+    String nombre; // Nombre del jugador de fútbol
+    int votos; // Votos recibidos en la aplicación
+    int id; // Número único identificativo
+
+    public Jugador(String nombre, int votos, int id) {
+        this.nombre = nombre;
+        this.votos = votos;
+        this.id = id;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ tarea2GitLidia/src/tarea2gitlidia/Tarea2GitLidia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2gitlidia;
+
+/**
+ *
+ * @author bicho
+ */
+public class Tarea2GitLidia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Jugador jug1 = new Jugador ("Lidia",2,5435);
+    }
+    
+}
